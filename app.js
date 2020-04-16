de8e15014411a7446a42bff9f2f072c8f14ab4a4
zlchai <zlchai@student.unimelb.edu.au>
2020-04-16 12:58:44 +1000

    first branch commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+
+const app = express();
+
+// use the body-parser middleware, which parses request bodies into req.body
+// support parsing of json
+app.use(bodyParser.json());
+// support parsing of urlencoded bodies (e.g. for forms)
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// GET home page
+app.get("/", (req, res) => {
+  res.send("<H1>Discussion Forum system</H1>");
+});
+
+
+
+// start app and listen for incoming requests on port 3000
+app.listen(3000, () => {
+  console.log("The library app is listening on port 3000!");
+});
---
+++ models/comment.js
@@ -0,0 +1,14 @@
+const comments = [
+  {
+    id: "10001",
+    first_name: "Jennifer",
+    last_name: "Robbins"
+  },
+  {
+    id: "10002",
+    first_name: "Evan",
+    last_name: "Hahn"
+  }
+];
+
+module.exports = comments;
