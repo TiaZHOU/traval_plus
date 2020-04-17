69fc0740de4e774639289a0731914cf87c5187cf
zlchai <zlchai@student.unimelb.edu.au>
2020-04-17 22:47:05 +1000

    Updates

--- models/comment.js
+++ models/comment.js
@@ -1,13 +1,19 @@
 const comments = [
   {
-    id: "10001",
+    id: "0001",
+    post: "Places to visit in Japan?",
+    comment: "I would check out...",
     first_name: "Jennifer",
-    last_name: "Robbins"
+    last_name: "Robbins",
+    created_date: "2020-01-14"
   },
   {
-    id: "10002",
+    id: "0002",
+    post: "Travelling tips for the USA",
+    comment: "Make sure you...",
     first_name: "Evan",
-    last_name: "Hahn"
+    last_name: "Hahn",
+    created_date: "2020-04-15"
   }
 ];
 
--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,12 @@
-const express
+const express = require("express");
+
+// create router
+const router = express.Router();
+
+// load/import the controllers
+const appController = require('../controllers/appController.js');
+const authController = require('../controllers/authController.js');
+const userController = require('../controllers/userController.js');
+const commentController = require('../controllers/commentController.js');
+
+module.exports = router;
