0aa7b74cf82da238730130d2911bf3a5f63672c3
ANUBARY <62126070+ANUBARY@users.noreply.github.com>
2020-04-29 22:21:13 +1000

    merge visaModel to master

---
+++ models/visaModel.js
@@ -0,0 +1,14 @@
+  const visa = [
+  {
+    id: "0001",
+    Country: "US",
+    visaRequirements: ["Doc1", "Doc2", "Doc3"]
+  },
+  {
+    id: "0002",
+    Country: "Singapore",
+    visaRequirements: ["Doc1", "Doc2", "Doc3"]
+  }
+];
+
+module.exports = visa;
