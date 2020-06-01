0495affe75f26cc1aca0176cce12eebf4a043502
ANUBARY <anubary@student.unimelb.edu.au>
2020-06-02 01:21:16 +1000

    updated task css

--- client/src/pages/Tasks/Tasks.js
+++ client/src/pages/Tasks/Tasks.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useTasks /*addTask, updateTask, deleteTask*/ } from "./TasksAPI";
 import './Tasks.css';
+import Footer from "../../components/Footer/Footer";
 
 export default function Tasks() {
     const { loading, tasks, error } = useTasks();
@@ -12,44 +13,51 @@ export default function Tasks() {
     }
 
     return (
-        <div className="container">
-            <div className="addTask">
-                <h3>Add New Task</h3>
-                <form>
-                    <label htmlFor="task_name">Task name:</label>
-                    <input type="text" placeholder="Task name" name="task_name"
-                        // value={id}
-                        // onChange={event => {
-                        //   setId(event.target.value);
-                        // }}
-                    />
-                    <br/>
-                    <label htmlFor="task_date">Task date and time:</label>
-                    <input type="datetime-local" name="task_date"
-                        // value={first_name}
-                        // onChange={event => {
-                        //   setFirstName(event.target.value);
-                        // }}
-                    />
-                    <br/>
-                    <label htmlFor="task_description">Task description:</label>
-                    <input type="text" placeholder="Task description (Optional)" name="task_description"
-                        // value={first_name}
-                        // onChange={event => {
-                        //   setFirstName(event.target.value);
-                        // }}
-                    />
-                    <br/>
-                    <br />
-                    <input type="submit" value="Submit"/>
-                </form>
+        <div>
+            <div className="task_background">
+
+                <h1>Plan Your Trip</h1>
+                <div className="container">
+                    <div className="addTask">
+                        <h3>Add New Task</h3>
+                        <form>
+                            <label htmlFor="task_name">Task name:</label>
+                            <input type="text" placeholder="Task name" name="task_name"
+                                // value={id}
+                                // onChange={event => {
+                                //   setId(event.target.value);
+                                // }}
+                            />
+                            <br/>
+                            <label htmlFor="task_date">Task date and time:</label>
+                            <input type="datetime-local" name="task_date"
+                                // value={first_name}
+                                // onChange={event => {
+                                //   setFirstName(event.target.value);
+                                // }}
+                            />
+                            <br/>
+                            <label htmlFor="task_description">Task description:</label>
+                            <input type="text" placeholder="Task description (Optional)" name="task_description"
+                                // value={first_name}
+                                // onChange={event => {
+                                //   setFirstName(event.target.value);
+                                // }}
+                            />
+                            <br/>
+                            <br />
+                            <input type="submit" value="Submit"/>
+                        </form>
+                    </div>
+                    <div className="taskList">
+                        <h3>Tasks List</h3>
+                        {tasks.map(task => (
+                            <Task key={task._id} {...task} />
+                        ))}
+                    </div>
+                </div>
             </div>
-            <div className="taskList">
-                <h3>Tasks List</h3>
-                {tasks.map(task => (
-                    <Task key={task._id} {...task} />
-                ))}
-            </div>
+        <Footer />
         </div>
     );
 }
