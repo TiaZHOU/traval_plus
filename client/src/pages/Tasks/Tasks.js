30258ff73fd949ef117ac49feafaccdf9c77c800
Angelinalim911 <61859285+Angelinalim911@users.noreply.github.com>
2020-06-04 12:13:30 +1000

    Working delete function in tasks API

--- client/src/pages/Tasks/Tasks.js
+++ client/src/pages/Tasks/Tasks.js
@@ -57,6 +57,7 @@ export default class Tasks extends Component {
     getTask = () => {
         axios.get(BASE_URL + '/tasks')
             .then((response) => {
+
                 const data = response.data;
                 this.setState({tasks: data, loading: false });
             })
@@ -68,12 +69,15 @@ export default class Tasks extends Component {
     deleteTask = (_id) => {
         axios.delete(BASE_URL + '/tasks/' + _id)
             .then(response => {
-                console.log(response.data);
-                this.getTask();
+               if(response.data != null) {
+                   this.setState({
+                       tasks: this.state.tasks.filter(task => task._id !== _id)
+                   });
+               }
             })
             .catch((error) => {
                 throw error.response.data
-            })
+            });
     };
 
     displayTask = (task) => {
@@ -81,10 +85,10 @@ export default class Tasks extends Component {
         if (!task) return null;
         return (
             <div className="task" key={_id}>
-                <i className="fas fa-times" onClick={this.deleteTask(_id)} />
+                <i className="fas fa-times"  onClick={ () => this.deleteTask(_id) }/>
                 <input type="checkbox" id={_id} />
                 <label htmlFor={_id}>
-                    <i className="fas fa-check" />
+                        <i className="fas fa-check" />
                     {taskName}
                     <span className="details">{taskDate}, {taskTime}</span>
                 </label>
@@ -104,19 +108,19 @@ export default class Tasks extends Component {
                         <header>Add New Task</header>
                         <form onSubmit={this.submitHandler}>
                             <div>
-                                <label htmlFor="taskName">Task name:</label>
+                                <label htmlFor="taskName">Task name</label>
                                 <input type="text" name="taskName" value={taskName} onChange={this.changeHandler} />
                             </div>
                             <div>
-                                <label htmlFor="taskDate">Task date:</label>
+                                <label htmlFor="taskDate">Task date</label>
                                 <input type="date" name="taskDate" value={taskDate} onChange={this.changeHandler} />
                             </div>
                             <div>
-                                <label htmlFor="taskTime">Task time:</label>
+                                <label htmlFor="taskTime">Task time</label>
                                 <input type="time" name="taskTime" value={taskTime} onChange={this.changeHandler} />
                             </div>
                             <div>
-                                <label htmlFor="taskDescription">Task description:</label>
+                                <label htmlFor="taskDescription">Task description</label>
                                 <input type="text" name="taskDescription" value={taskDescription} onChange={this.changeHandler} />
                             </div>
                             <button className="button">Submit</button>
@@ -129,6 +133,10 @@ export default class Tasks extends Component {
                             tasks.map(task => (
                                 this.displayTask(task)
                             ))}
+
+                        {this.state.tasks.length == 0 ?
+                            <p>Add a task!</p> : <p/>
+                        }
 
                         <footer />
                     </div>
